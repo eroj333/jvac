2880bea54a9e0afcebb628b9f4330e87e6640bcf
Amit <studenton.com@gmail.com>
2016-06-05 13:59:25 +0545

    Fixed styling

--- teser.py
+++ teser.py
@@ -1,8 +1,8 @@
-from PIL import Image 
-from pytesseract import image_to_string 
+from PIL import Image
+from pytesseract import image_to_string
 
-image_file = "tt.png";
-img = Image.open(image_file);
-text = image_to_string(img);
-with open('output.txt','w') as file:
-	file.write(text)
+image_file = "tt.png"
+img = Image.open(image_file)
+text = image_to_string(img)
+with open('output.txt', 'w') as file:
+    file.write(text)
