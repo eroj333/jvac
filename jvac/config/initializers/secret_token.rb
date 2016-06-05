6806884bce1e910c8c4446e0d2fbbb83319c2b3a
Kamal <kamalpaneru.15@gmail.com>
2016-06-05 12:58:00 +0545

    Initial commit for application layout

---
+++ jvac/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jvac::Application.config.secret_token = 'daafb7ab23c61008816221b15be816d850b7e7afb71dd0aa195d6ec88ed91e3164171fc8cd96ec264427417c0277941d27b08900c0c4d85ecbc004ba1284e8c7'
