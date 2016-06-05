a0acc24332523dda5fe4f765412f9eb87d82a179
Kamal <kamalpaneru.15@gmail.com>
2016-06-05 13:16:53 +0545

    Added controllers for home features and about

---
+++ jvac/app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def features
+  end
+
+  def about
+  end
+end
--- jvac/config/routes.rb
+++ jvac/config/routes.rb
@@ -1,4 +1,10 @@
 Jvac::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/features"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
