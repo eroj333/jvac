a2c023c4e53dde6c5ab1b92d2bc647fb37ccfb12
Amit <studenton.com@gmail.com>
2016-06-06 13:06:24 +0545

    Added basic template for the installation script.

--- installer.sh
+++ installer.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+
+# Install the requirements for the project.
 
 # Install ImageMagick
 apt-get -y install imagemagick
+
+# Install OpenCV2
+apt-get -y install 
+
+# Install the python binding for opencv
+
+
+
+# Install Image Magick
+
+
+# Install Tesseract
+
+# Install the python binding for tesseract
+
+
