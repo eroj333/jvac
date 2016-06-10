6fc4c1e82f0f7a41b78510bcdc6ec6deb8a32ed7
Kamal <kamalpaneru.15@gmail.com>
2016-06-10 11:35:02 +0545

    Add custom js script

--- jvac/app/assets/javascripts/min.js
+++ jvac/app/assets/javascripts/min.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('.menu').click(function(e){
       e.stopPropagation();
         $('nav').toggleClass('active');
-        $('.button_smaller').toggleClass('active')
+        $('style').toggleClass ('active')
+
   });
 });
