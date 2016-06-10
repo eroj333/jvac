342ee96491383356d71b32b91bb62347b124fb6e
Amit <studenton.com@gmail.com>
2016-06-10 14:36:15 +0545

    Remove unused selector

--- jvac/app/assets/javascripts/min.js
+++ jvac/app/assets/javascripts/min.js
@@ -2,7 +2,5 @@ $(document).ready(function(){
   $('.menu').click(function(e){
       e.stopPropagation();
         $('nav').toggleClass('active');
-        $('style').toggleClass ('active')
-
   });
 });
