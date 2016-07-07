f349c3dbe01d3ec6db719ff30fa8b739086ea39c
Amit Chaudhary <studenton.com@gmail.com>
2016-07-07 22:18:21 +0545

    Fix linter issues

--- jvac/app/models/user.rb
+++ jvac/app/models/user.rb
@@ -1,25 +1,24 @@
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible  :name , :email ,:password , :password_confirmation
+  attr_accessible  :name , :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name , :presence => true,
-                    :length => { :maximum => 50}
+                    :length => { :maximum => 50 }
 
   validates :email, :presence => true,
-                    :format => { :with => email_regex},
-                    :uniqueness => { :case_sensitive => false}
+                    :format => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
 
-  validates :password , :presence => true,
-                        :confirmation => true,
-                        :length => { :within =>  6..40}
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within =>  6..40 }
 
   before_save :encrypt_password
 
   def has_password?(submitted_password)
-      encrypted_password = encrypt( submitted_password)
-
+    encrypted_password = encrypt(submitted_password)
   end
 
   def self.authenticate(email, submitted_password)
@@ -46,9 +45,4 @@ class User < ActiveRecord::Base
   def secure_hash(string)
     Digest::SHA2.hexdigest(string)
   end
-
-
-
-
-
 end
