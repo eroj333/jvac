a23aa4fe70fe068e9c598df95c4a83fba18da00a
Kamal <kamalpaneru.15@gmail.com>
2016-10-01 19:51:35 +0545

    Authentication for user

--- jvac/app/models/user.rb
+++ jvac/app/models/user.rb
@@ -22,10 +22,12 @@ class User < ActiveRecord::Base
   end
 
   def self.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(submitted_password)
+    @user = find_by_email(email)
+    return nil if @user.nil?
+    return user if @user.has_password?(submitted_password)
   end
+
+
 
   private
 
