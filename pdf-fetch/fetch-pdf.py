dca342e391529a7172ae6b71a5938b8f4a26c0b7
Amit Chaudhary <studenton.com@gmail.com>
2016-10-01 17:07:42 +0545

    Add progress bar to download

--- pdf-fetch/fetch-pdf.py
+++ pdf-fetch/fetch-pdf.py
@@ -5,24 +5,36 @@ Fetch the epaper for current day.
 import urllib2
 from datetime import date
 
-url = 'http://epaper.ekantipur.com/epaper/the-kathmandu-post/'
 
+def download_file(url, location=''):
+    file_name = url.split('/')[-1]
+    location += file_name
+    u = urllib2.urlopen(url)
+    f = open(location, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print " Downloading: %s \n Size: %d MB" % (file_name, file_size / 1048576)
 
-def download_pdf(url):
-    pdf_file = urllib2.urlopen(url).read()
-    with open(output_name, 'wb') as file:
-        file.write(pdf_file)
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
 
-today = date.today()
-year = today.year
-month = today.month
-day = today.day
-if month < 10:
-    month = '0' + str(month)
-if day < 10:
-    day = '0' + str(day)
-current = "{}-{}-{}".format(year, month, day)
-url += '{}/{}.pdf'.format(current, current)
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"Completed: %3.2f%%" % (file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
+    f.close()
 
-output_name = 'Epaper-' + current + '.pdf'
-download_pdf(url)
+if __name__ == '__main__':
+    url = 'http://epaper.ekantipur.com/epaper/the-kathmandu-post/'
+    today = date.today()
+    year = today.year
+    month = today.month
+    day = today.day
+    current = "{}-{:02}-{:02}".format(year, month, day)
+    url += '{}/{}.pdf'.format(current, current)
+    download_file(url, '/home/anon/Desktop/')
